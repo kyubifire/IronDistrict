5b725a98f65b6eb3fbde29c9cf0862837dd38a40
Gabrielle <ransom514@gmail.com>
2017-09-10 03:34:43 -0500

    Dialogue Cut Scene is in progress

---
+++ RPG Project/Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour {
+	string characterName = "";
+	Sprite characterSprite = null;
+	public Sprite[] characterPoses = null;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
