e15d1ffdde1cd4b016b94f4d27ea3a1fb76b15d6
Nilo Lisboa <nilo_bl@hotmail.com>
2017-09-10 18:24:41 -0300

    Added most dialogue code

--- RPG Project/Assets/Scripts/DialogueManager.cs
+++ RPG Project/Assets/Scripts/DialogueManager.cs
@@ -1,28 +1,86 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
+﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueManager : MonoBehaviour {
-	public GameObject dialogueBox;
-	public Text dialogueText;
 
-	public bool dialogueActive;
+    DialogueParser parser;
 
-	void Start () {
-	
-	}
+    public string speech;
+    public string characterName;
+    public int lineNum;
+    int pose;
+    string position;
+    List<Button> buttons = new List<Button>();
 
-	void Update() {
-		if (dialogueActive && Input.GetKeyDown (KeyCode.Space)) {
-			dialogueBox.SetActive (false);
-			dialogueActive = false;
-		}
-	}
+    public Text dialBox;
+    public Text nameBox;
+    // Use this for initialization
+    void Start() {
+        //inits
+        speech = "";
+        characterName = "";
+        pose = 0;
+        position = "L";
+        parser = GameObject.Find("Parser").GetComponent<DialogueParser>();
+        lineNum = 0;
+    }
 
-	public void showBox(string dialogue) {
-		dialogueActive = true;
-		dialogueBox.SetActive(true);
-		dialogueText.text = dialogue;
-	}
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            showSpeech();
+            lineNum++;
+        }
+        updateUI();
+    }
+
+    public void showSpeech() {
+        resetImages();
+        parseLine();
+    }
+
+    void resetImages() {
+        if (characterName != "") {
+            GameObject character = GameObject.Find(characterName);
+            SpriteRenderer currSprite = character.GetComponent<SpriteRenderer>();
+            currSprite.sprite = null;
+        }
+    }
+
+    void parseLine() {
+        characterName = parser.getName(lineNum);
+        speech = parser.getContent(lineNum);
+        pose = 0;
+        position = parser.getPosition(lineNum);
+        displayImages();
+    }
+
+    void displayImages() {
+        if (characterName != "") {
+            GameObject character = GameObject.Find(characterName);
+            setSpritePos(character);
+
+            SpriteRenderer currSprite = character.GetComponent<SpriteRenderer>();
+            currSprite.sprite = character.GetComponent<Character>().characterPoses[pose];
+        }
+    }
+
+    void setSpritePos(GameObject spriteObj) {
+        if (position == "L")
+        {
+            spriteObj.transform.position = new Vector2(-6, 200);
+        }
+        else if (position == "R")
+        {
+            spriteObj.transform.position = new Vector2(6, 200);
+        }
+        spriteObj.transform.position = new Vector2(spriteObj.transform.position.x, spriteObj.transform.position.y);
+    }
+
+    void updateUI() {
+        dialBox.text = speech;
+        nameBox.text = characterName;
+    }
 }
