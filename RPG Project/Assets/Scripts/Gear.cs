792601331d2e0fc4f0b7127840b1004134a570e3
Gabrielle Ransom <ransom514@gmail.com>
2017-09-17 23:15:36 -0700

    Still setting up gear motion. Need to seperate Gear.cs for health and attack into seperate scripts

--- RPG Project/Assets/Scripts/Gear.cs
+++ RPG Project/Assets/Scripts/Gear.cs
@@ -1,37 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 public class Gear : MonoBehaviour {
-
-	public bool isAlive;
+	public bool isAlive;			// check if character is dead.  If so, disable gears for that character
 	private string attackType;
 	public Ally self;
 	Animator animator;
 
+	public int startingHealth = 100;
+	public int currentHealth;
+	public Slider healthSlider;
+	public Image damageImage;
+	public float flashSpeed = 5f;
+	public Color flashColor = new Color(1f, 0f, 0f, 0.1f);		// red
 
-
+	AudioSource playerAudio;
+	bool isDead;				// check for if player is dead or not
+	bool damaged;
 
 	// Use this for initialization
 	void Start () {
 		isAlive = true;
 		attackType = self.attackType;
 		animator = GetComponent <Animator>();
+		// set initial health of player
+		currentHealth = startingHealth;
 		updateGearSprite();
 	}
 
-	void updateGearSprite(){
+	void updateGearSprite() {
 		attackType = self.attackType;
 		if (attackType == "green") {
-
 			animator.SetInteger ("gearType", 0);
 		} else if (attackType == "blue") {
-
 			animator.SetInteger ("gearType", 1);
 		} else if (attackType == "red") {
 			animator.SetInteger ("gearType", 2);
 		}
+
 		if (!isAlive) {
 			animator.SetBool ("dead", true);
 		}
@@ -39,31 +47,32 @@ public class Gear : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// When the player or an ally is hit the screen will  flash red to signify damage
+		if (damaged) {
+			damageImage.color = flashColor;
+		} else {
+			damageImage.color = Color.Lerp (damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+		}
+		damaged = false;
+
 		updateGearSprite();
 		if (self.currentHealth < 0) {
 			isAlive = false;
 		}
-			
-		
 	}
-
-
-	public void battle(Gear other){
+		
+	public void battle(Gear other) {
 		//if self.attackType == red
 		if (attackType == "red" && other.attackType == "blue") {
 			//if bad matchup
 			badAttack (other.self);
-
 		} else if (attackType == "red" && other.attackType == "red") {
 			//if eh matchup
 			goodAttack (other.self);
-
 		} else if (attackType == "red" && other.attackType == "green") {
 			//if super matchup
 			superAttack (other.self);
-
 		}
-
 		//if self.attackType == blue
 		else if (attackType == "blue" && other.attackType == "green") {
 			//if bad matchup
@@ -71,62 +80,60 @@ public class Gear : MonoBehaviour {
 		} else if (attackType == "blue" && other.attackType == "blue") {
 			//if eh matchup
 			goodAttack (other.self);
-
 		} else if (attackType == "blue" && other.attackType == "red") {
 			//if super matchup
 			superAttack (other.self);
-
-
 		}
 		//if self.attackType == green
 		else if (attackType == "green" && other.attackType == "red") {
 			//if bad matchup
 			badAttack (other.self);
-				
 		} else if (attackType == "green" && other.attackType == "green") {
 			//if eh matchup
 			goodAttack (other.self);
-
 		} else if (attackType == "green" && other.attackType == "blue") {
 			//if super matchup
 			superAttack (other.self);
-
 		} else {
 			Debug.Log ("You're A dipSHIt check your type names");
 		}
-			
-		
 	}
-
 
 	void badAttack(Ally other){
 	//bad attack hurts self
-
-		self.damage(other.currentDamage);
-
-
-		Debug.Log (other.name + " Bad Attacks " + self.name);
-
-
+	self.damage(other.currentDamage);
+	Debug.Log (other.name + " Bad Attacks " + self.name);
 	}
 
-	void goodAttack(Ally other){
+	void goodAttack(Ally other) {
 	//good Attack hurts enemy and ally
-		other.damage(self.currentDamage);
-		self.damage(other.currentDamage);
-
-		Debug.Log (self.name + " Good Attacks " + other.name);
+	other.damage(self.currentDamage);
+	self.damage(other.currentDamage);
+	Debug.Log (self.name + " Good Attacks " + other.name);
 	}
 
 	void superAttack(Ally other){
 	//super attack hurts enemy
-
-		other.damage (self.currentDamage);
-
-
-		Debug.Log (self.name + " Super Attacks " + other.name);
-
+	other.damage (self.currentDamage);
+	Debug.Log (self.name + " Super Attacks " + other.name);
 	}
 
+	public void takeDamage (int amountOfDamage) {
+		damaged = true;
+		currentHealth -= amountOfDamage;
+		healthSlider.value = currentHealth;
+		playerAudio.Play ();
 
+		if (currentHealth <= 0 && isDead) {
+			playerDeath ();
+		}
+	}
+
+	void playerDeath() {
+		isDead = true;
+		//		anim.SetTrigger ("Die");
+		//playerController.enabled = false;
+		//playerAudio.clip = deatchClip;
+		//playerAudio.Play ();
+	}
 }
--- RPG Project/Assets/Scripts/GearManagement.cs
+++ RPG Project/Assets/Scripts/GearManagement.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GearManagement : MonoBehaviour {
-
-
 	public List<Gear> yourGears;
 	public List<Gear> enemyGears;
 	public Text txt;
 	bool canSwitch;
 	private bool canBattle;
 	private bool isSwitched;
+
 	// Use this for initialization
 	void Start () {
 		isSwitched = false;
@@ -22,23 +21,15 @@ public class GearManagement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-
 		if (Input.GetKeyDown (KeyCode.R)) {
 			Application.LoadLevel (Application.loadedLevel);
 		}
-
-
-
 		if (Input.GetKeyDown (KeyCode.Return) && canBattle == true) {
 			canBattle = false;
 			matchStart();
 		}
-
-
 		//does not work if you cant switch the  pieces
 		if (canSwitch == true) {
-
 			if (Input.GetKeyDown (KeyCode.Space)) {
 				if (!isSwitched) {
 					//isSwitched = true;
@@ -52,13 +43,11 @@ public class GearManagement : MonoBehaviour {
 						yourGears [1].self.attackType = tempType;
 					}            
 				}
-
-
 			}
 		}
 	}
 
-	void checkGearDeath(){
+	void checkGearDeath() {
 		//checks if your character has no health
 		for (int g = 0; g < yourGears.Count; g++){
 			if(yourGears[g].self.currentHealth <= 0){
@@ -72,7 +61,7 @@ public class GearManagement : MonoBehaviour {
 		}
 	}
 
-	void checkHealth(){
+	void checkHealth() {
 		//debug if health is changing
 		for (int g = 0; g < yourGears.Count; g++){
 			Debug.Log (yourGears [g].self.allyName + ": " + yourGears [g].self.currentHealth);
@@ -81,17 +70,11 @@ public class GearManagement : MonoBehaviour {
 			Debug.Log (enemyGears [g].self.allyName + ": " + enemyGears [g].self.currentHealth);
 		}
 	}
-
-
-
-	public void matchStart(){
 		
+	public void matchStart() {
 		//when the battle phase of minigame begins
-
-
 		canSwitch = false;
 		//stops you from switching the pieces mid battle
-
 		int currentGear = 0;
 		//goes through both arrays 
 		while (currentGear < yourGears.Count && currentGear < enemyGears.Count) {
@@ -99,12 +82,7 @@ public class GearManagement : MonoBehaviour {
 			checkGearDeath ();
 			currentGear = currentGear + 1;
 		} 
-
-
-
-
 		int secondGear = 0;
-
 		//if there are more team members than enemies
 		if (yourGears.Count > enemyGears.Count) {
 			while (currentGear < yourGears.Count) {
@@ -116,11 +94,8 @@ public class GearManagement : MonoBehaviour {
 				currentGear = currentGear + 1;
 				secondGear = secondGear + 1;
 			}
-		
-
-
 		//if there are more enmeies than team members 
-		}else if (yourGears.Count< enemyGears.Count) {
+		} else if (yourGears.Count< enemyGears.Count) {
 			while (currentGear < enemyGears.Count) {
 				if (secondGear >= yourGears.Count) {
 					secondGear = 0;
@@ -130,7 +105,6 @@ public class GearManagement : MonoBehaviour {
 				currentGear = currentGear + 1;
 				secondGear = secondGear + 1;
 			}
-
 		}
 
 		checkGearDeath ();
@@ -148,6 +122,4 @@ public class GearManagement : MonoBehaviour {
 
 		}
 	}
-
-
 }
